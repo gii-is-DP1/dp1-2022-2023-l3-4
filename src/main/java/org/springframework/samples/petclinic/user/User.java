4e0935bdce163e98f19a41e64f7f31bd75352140
josgarber6 <josgarber6@alum.us.es>
2022-10-12 18:35:42 +0200

    Cambios en User.java entidad nueva

--- src/main/java/org/springframework/samples/petclinic/user/User.java
+++ src/main/java/org/springframework/samples/petclinic/user/User.java
@@ -6,7 +6,9 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +19,21 @@ import lombok.Setter;
 @Table(name = "users")
 public class User{
 	@Id
+	@NotEmpty
 	String username;
 	
+	@NotEmpty
 	String password;
+
+	public enum status {
+		ONLINE,
+		OFFLINE
+	}
+
+	@OneToOne
+	private String code;
+
+	String description;
 	
 	boolean enabled;
 	

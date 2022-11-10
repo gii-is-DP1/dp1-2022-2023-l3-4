f4ff7bc5dbdd153d05ccbd7e9ba295e2ba9547b7
lucperrom1 <lucperrom1@alum.us.es>
2022-11-10 16:49:37 +0100

    corrección cards y método repartirCartas

--- src/main/java/org/springframework/samples/petclinic/card/CardService.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -16,21 +16,8 @@
 package org.springframework.samples.petclinic.card;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,4 +47,14 @@ public class CardService {
 		return cardRepository.findAll().stream().filter(x->x.getPlayed()).collect(Collectors.toList());
 
 	}
+	@Transactional(readOnly = true)
+		public Optional<Card> findCard(Integer i){
+			return cardRepository.findById(i);
+		}
+
+	@Transactional
+		public Card save(Card card){
+			return cardRepository.save(card);	
+		}
+	
 }

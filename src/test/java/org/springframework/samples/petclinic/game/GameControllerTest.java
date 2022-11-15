0a9caa2005448fb9eace01329e19cbb0eb89de9b
mariams73 <marmarmar73@alum.us.es>
2022-11-15 23:44:16 +0100

    Avances en GameControllerTest y cambios en el método reset

--- src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
@@ -1,27 +1,36 @@
 package org.springframework.samples.petclinic.game;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Any;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.card.CardService;
+import org.springframework.samples.petclinic.card.GenericCardRepository;
+import org.springframework.samples.petclinic.card.GenericCardService;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.gamePlayer.GamePlayerService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.security.test.context.support.WithMockUser;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Optional;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 
@@ -34,22 +43,49 @@ public class GameControllerTest {
 
     @MockBean
     private GameService gameService;
+
+    @MockBean
+    private GamePlayerService gamePlayerService;
+
+    @MockBean
+    private CardService cardService;
+
+    @MockBean
+    private GenericCardService gCardService;
+
+    @Autowired
+    private GameController gc;
+
+    @MockBean
     private GameRepository gameRepo;
 
-    @Before
-    void setup() {
+    @MockBean
+    private GenericCardRepository gCardRepo;
+
+    Game game;
+    @BeforeEach
+    public void setUp() throws Exception {
+        Game game = new Game();
+        game.setId(70);
+        Optional<Game> theGame = Optional.of(game);
         gameService = new GameService(gameRepo);
+        when(gameRepo.findById(any(Integer.class))).thenReturn(theGame);
+    }
+
+    @WithMockUser
+    @Test
+    public void testGameListing() throws Exception {
+        mockMvc.perform(get("/games"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("games/listing"))
+            .andExpect(model().attributeExists("games"));
     }
     /* 
-    @WithMockUser(value = "spring")
+    @WithMockUser
     @Test
-    public void test1() throws Exception {
-        testHTTPGameList();
+    public void testReset() throws Exception {
+        gc.reset();
+        assertTrue(gCardRepo.findAll().size()==68);
     }
     */
-    public void testHTTPGameList() throws Exception{
-        mockMvc.perform(get("/games"))
-        .andExpect(status().isOk());
-    }
-    
 }

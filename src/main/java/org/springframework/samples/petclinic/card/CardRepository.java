39031660830e7034711d9b1d3be056e38892cf6d
lucperrom1 <lucperrom1@alum.us.es>
2022-11-15 18:32:20 +0100

    srry mistake

--- src/main/java/org/springframework/samples/petclinic/card/CardRepository.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardRepository.java
@@ -12,7 +12,7 @@ public interface CardRepository extends CrudRepository<Card, Integer> {
     List<Card> findAll();
     Optional<Card> findById(Integer i);
     Card save(Card card);
-    @Query("SELECT c FROM Card c WHERE c.body() AND c.gamePlayer_id = :gamePlayerId")
+    @Query("SELECT c FROM Card c WHERE c.body=1 AND c.gamePlayer.id = :gamePlayerId")
     List<Card> getBodyFromAGamePlayer(Integer gamePlayerId);
     
 }

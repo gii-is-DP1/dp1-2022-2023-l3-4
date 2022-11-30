7f6f089927edcc363ea90c98a2d69ed40a8d842b
josgarber6 <josgarber6@alum.us.es>
2022-11-30 19:05:09 +0100

    genera tablero fino

--- src/main/java/org/springframework/samples/virus/game/GameController.java
+++ src/main/java/org/springframework/samples/virus/game/GameController.java
@@ -161,7 +161,7 @@ public class GameController {
 			bodies.put(gp, gp.getCards().stream().filter(x->x.getBody()).collect(Collectors.toList()));
 		}
 		model.put("bodies", bodies);
-		return "games/game";
+		return model;
 	}
 
 	//Cambio de turno

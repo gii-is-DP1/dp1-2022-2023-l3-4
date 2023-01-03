aadde26a5d0cef2968e5ed0df10369a57f72cca3
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-01-03 14:16:58 +0100

    nueva estructura de room para guardar historial de room creadas

--- src/main/java/org/springframework/samples/petclinic/room/Room.java
+++ src/main/java/org/springframework/samples/petclinic/room/Room.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.room;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.Max;
@@ -36,10 +37,12 @@ public class Room extends BaseEntity{
     @NotNull
     Boolean isPrivate;
 
+    Boolean active;
+
     @OneToMany(mappedBy="room")
     private Collection<Player> players;
 
-    @OneToOne(optional = false)
+    @ManyToOne(optional = false)
     private Player host;
 
     String password;

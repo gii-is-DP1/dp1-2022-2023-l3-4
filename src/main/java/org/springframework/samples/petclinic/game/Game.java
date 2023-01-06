6dd65487ecd0a6ad92b05303545b55e87f64d159
Francisco Benítez <frabenrui1@alum.us.es>
2023-01-06 13:06:31 +0100

    cambios en gameplayer

--- src/main/java/org/springframework/samples/petclinic/game/Game.java
+++ src/main/java/org/springframework/samples/petclinic/game/Game.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;

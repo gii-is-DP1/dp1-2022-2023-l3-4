5bb12eedbcacd58c2d8f9d875dbfe5a8ed25736b
Francisco Benítez <frabenrui1@alum.us.es>
2023-01-07 15:15:08 +0100

    actualizado player controller test

--- src/test/java/org/springframework/samples/petclinic/player/PlayerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/player/PlayerControllerTest.java
@@ -74,12 +74,11 @@ public class PlayerControllerTest {
             .andExpect(model().attributeExists("player"))
             .andExpect(model().attributeExists("gameplayer"))
             .andExpect(model().attributeExists("games"))
+            .andExpect(model().attributeExists("statistics"))
             .andExpect(model().attributeExists("currentPage"))
             .andExpect(model().attributeExists("totalPages"))
-            .andExpect(model().attributeExists("numGamesPlayed"))
-            .andExpect(model().attributeExists("totalTimePlayed"))
-            .andExpect(model().attributeExists("numGamesWon"));
-
+            .andExpect(model().attributeExists("statistics"))
+            .andExpect(model().attributeExists("totalTimePlayed"));
     }
 
     @WithMockUser

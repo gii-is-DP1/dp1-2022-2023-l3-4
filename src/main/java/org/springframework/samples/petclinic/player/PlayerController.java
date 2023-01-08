881a3d34bd6a7792af6dc9361312209aaf60edfd
Francisco Benítez <frabenrui1@alum.us.es>
2023-01-08 11:26:09 +0100

    Ya se muestran las cartas usadas en cada juego, game tiene un winner

--- src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
@@ -17,9 +17,6 @@ package org.springframework.samples.petclinic.player;
 
 import java.time.Duration;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
@@ -41,7 +38,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;

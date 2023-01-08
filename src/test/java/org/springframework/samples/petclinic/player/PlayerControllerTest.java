5552655b3adf3bc8dda4548cda4aa25326da1e1b
Francisco Benítez <frabenrui1@alum.us.es>
2023-01-08 17:58:55 +0100

    Player controller terminado (test incluido)

--- src/test/java/org/springframework/samples/petclinic/player/PlayerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/player/PlayerControllerTest.java
@@ -29,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
@@ -54,12 +55,15 @@ public class PlayerControllerTest {
     @BeforeEach
     public void setUp() {
         Player mockPlayer = new Player();
+
         GamePlayer mockGamePlayer = new GamePlayer();
         Page<Game> mockGames = new PageImpl<>(new ArrayList<>());
         when(authenticationService.getPlayer()).thenReturn(mockPlayer);
         when(authenticationService.getGamePlayer()).thenReturn(mockGamePlayer);
         when(gameService.findGamesByGameplayerPaged(eq(mockGamePlayer), any())).thenReturn(mockGames);
         when(gameService.humanReadableDuration(any())).thenReturn("string");
+        when(gameService.getNumGamesPlayed(mockGamePlayer)).thenReturn(10);
+        when(gameService.getNumGamesWon(mockGamePlayer)).thenReturn(5);
     }
     
     @WithMockUser
@@ -73,8 +77,10 @@ public class PlayerControllerTest {
             .andExpect(model().attributeExists("games"))
             .andExpect(model().attributeExists("currentPage"))
             .andExpect(model().attributeExists("totalPages"))
-            .andExpect(model().attributeExists("statistics"))
-            .andExpect(model().attributeExists("totalTimePlayed"));
+            .andExpect(model().attributeExists("numGamesPlayed"))
+            .andExpect(model().attributeExists("totalTimePlayed"))
+            .andExpect(model().attributeExists("numGamesWon"));
+
     }
 
     @WithMockUser
@@ -98,5 +104,74 @@ public class PlayerControllerTest {
         .andExpect(status().isOk())
         .andExpect(view().name("player/playerProfile"))
         .andExpect(model().attributeExists("message"));
+    }
+
+    @WithMockUser
+    @Test
+    public void testPlayerProfileEditEmptyFirstName() throws Exception {
+        mockMvc.perform(post("/player/me/edit")
+        .with(csrf())
+        .param("firstName", "")
+        .param("lastName", "Lopez")
+        .param("status", "true")
+        .param("description", "nueva descripcion."))
+        .andExpect(status().isOk())
+        .andExpect(view().name("player/createOrUpdateProfileForm"))
+        .andExpect(model().hasErrors());
+    }
+
+    @WithMockUser
+    @Test
+    public void testPlayerProfileEditEmptyLastName() throws Exception {
+        mockMvc.perform(post("/player/me/edit")
+        .with(csrf())
+        .param("firstName", "Emmanuel")
+        .param("lastName", "")
+        .param("status", "true")
+        .param("description", "nueva descripcion."))
+        .andExpect(status().isOk())
+        .andExpect(view().name("player/createOrUpdateProfileForm"))
+        .andExpect(model().hasErrors());
+    }
+
+    @WithMockUser
+    @Test
+    public void testSearch() throws Exception {
+        mockMvc.perform(get("/player/search"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("player/searchPlayer"))
+        .andExpect(model().attributeExists("player"));
+    }
+
+    @WithMockUser
+    @Test
+    public void testSearchPost() throws Exception {
+        Player mockPlayer = new Player();
+        Player anotherPlayer = new Player();
+        List<Player> players = new ArrayList<>();
+        players.add(mockPlayer);
+        players.add(anotherPlayer);
+
+        when(playerService.getPlayersByUsername("frabenrui1")).thenReturn(players);
+
+        mockMvc.perform(post("/player/search")
+        .with(csrf())
+        .param("user.username", "frabenrui1"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("player/playerListing"))
+        .andExpect(model().attributeExists("players"))
+        .andExpect(model().attribute("players", players));
+    }
+
+    @WithMockUser
+    @Test
+    public void testSearchNullUsername() throws Exception {
+        when(playerService.getPlayersByUsername("")).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(post("/player/search")
+        .with(csrf()))
+        .andExpect(status().isOk())
+        .andExpect(view().name("player/searchPlayer"))
+        .andExpect(model().hasErrors());
     }
 }

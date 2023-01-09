64503f9891cbdc074cab067df3b5ca0de29ed035
JuanAntonioJimenezdelVillar <juajimdel2@alum.us.es>
2023-01-09 17:38:53 +0100

    RoomRepository tests actualizado

--- src/test/java/org/springframework/samples/petclinic/room/RoomRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/room/RoomRepositoryTest.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.samples.petclinic.player.PlayerRepository;
 
 
 
@@ -16,19 +17,29 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 public class RoomRepositoryTest {
     @Autowired
     RoomRepository roomRepository;
+    
+    @Autowired
+    PlayerRepository playerRepository;
 
     @Test
-    public void testFindRoomsByRoomName(){
-        Collection<Room> rooms = roomRepository.findRoomsByRoomName("sala");
+    public void testFindRoomsByRoomName() {
+        Collection<Room> rooms = roomRepository.findRoomsByRoomName("prueba");
         assertNotNull(rooms, "El repositorio ha devuelto una lista nula");
-        assertEquals(2,rooms.size(),"Faltan datos de inicialización");
+        assertEquals(1,rooms.size(),"Faltan datos de inicialización");
     }
 
     @Test
-    public void testFindByRoomName(){
-        Room room = roomRepository.findByRoomName("sala1");
+    public void testFindByRoomName() {
+        Room room = roomRepository.findByRoomName("prueba");
         assertNotNull(room, "El repositorio ha devuelto una lista nula");
-        assertEquals("sala1", room.getRoomName(), "El nombre de la sala buscada no coincide con la devuelta del repositrio");
+        assertEquals("prueba", room.getRoomName(), "El nombre de la sala buscada no coincide con la devuelta del repositrio");
+    }
+
+    @Test
+    public void testfindRoomByHost() {
+        Room room = roomRepository.findRoomByHost(playerRepository.findById(5).get());
+        assertNotNull(room, "La sala debe tener un host asignado");
+        assertEquals("prueba", room.getRoomName(), "El nombre de la sala no coincide con la del host");
     }
 
 }

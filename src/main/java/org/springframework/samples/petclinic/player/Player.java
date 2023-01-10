25fdcf71a128aa007cf2efb9ed8056476290cee1
josgarber6 <josgarber6@alum.us.es>
2023-01-10 11:54:03 +0100

    Little import fix achievement in player

--- src/main/java/org/springframework/samples/petclinic/player/Player.java
+++ src/main/java/org/springframework/samples/petclinic/player/Player.java
@@ -34,7 +34,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.samples.petclinic.achievements.Achievement;
 import org.springframework.samples.petclinic.friendRequest.Friend;
 import org.springframework.samples.petclinic.gamePlayer.GamePlayer;
 import org.springframework.samples.petclinic.model.Person;

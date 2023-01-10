46b3f744a770de7fb177c71ecefdaee92f54f6f1
josgarber6 <josgarber6@alum.us.es>
2023-01-10 13:53:57 +0100

    Fixing issues while testing with auditin

---
+++ src/main/java/org/springframework/samples/petclinic/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+  
+}
--- src/main/java/org/springframework/samples/petclinic/VirusApplication.java
+++ src/main/java/org/springframework/samples/petclinic/VirusApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.samples.petclinic.player.SpringSecurityAuditorAware;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication()
 public class VirusApplication {
 

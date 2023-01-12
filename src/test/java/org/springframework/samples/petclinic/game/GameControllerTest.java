3323c63709f1b3e807e8ec5ba8f12af06c3fbedb
josgarber6 <josgarber6@alum.us.es>
2023-01-12 18:59:59 +0100

    deleteGame Test

--- src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.card.CardService;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.gamePlayer.GamePlayer;
 import org.springframework.samples.petclinic.gamePlayer.GamePlayerService;
+import org.springframework.samples.petclinic.room.Room;
 import org.springframework.samples.petclinic.room.RoomService;
 import org.springframework.samples.petclinic.util.AuthenticationService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -17,9 +19,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.samples.petclinic.card.GenericCardRepository;
 import org.springframework.samples.petclinic.card.GenericCardService;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
@@ -27,6 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = GameController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, 
 classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class GameControllerTest {
+
+    private static final Integer TEST_GAME_ID = 1;
 
     @Autowired
     private MockMvc mockMvc; 
@@ -70,6 +78,22 @@ public class GameControllerTest {
             .andExpect(view().name("games/terminateGameListing"))
             .andExpect(model().attributeExists("games"));
 
+    }
+
+    @WithMockUser(value = "admin1", password = "4dm1n", roles = "ADMIN")
+    @Test
+    public void testDeleteTerminatedGames() throws Exception {
+        Game mockGame = new Game();
+        mockGame.setId(TEST_GAME_ID);
+        mockGame.setRoom(new Room());
+        mockGame.setGamePlayer(new ArrayList<>());
+        mockGame.setCards(new ArrayList<>());
+        mockGame.setWinner(new GamePlayer());
+        
+        mockMvc.perform(get("/games/{gameId}/delete", TEST_GAME_ID))
+            .andExpect(status().isOk())
+            .andExpect(view().name("/terminateGames"))
+            .andExpect(model().attributeExists("message"));
     }
 
 

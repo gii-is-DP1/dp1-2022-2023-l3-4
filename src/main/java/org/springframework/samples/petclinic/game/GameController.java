cd4c7e1ce54f9762ff25cfe20debc04354ad222a
Francisco Benítez <frabenrui1@alum.us.es>
2023-01-14 00:53:10 +0100

    eliminado save gameplayer en playVirus

--- src/main/java/org/springframework/samples/petclinic/game/GameController.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameController.java
@@ -285,11 +285,9 @@ public class GameController {
 				cardService.infect(c_organ, c_virus);
 				cardService.save(c_virus);
 				cardService.save(c_organ);
-				gamePlayerService.save(c_virus.getGamePlayer());
-				gamePlayerService.save(c_organ.getGamePlayer());
 				if(old_card!=null)cardService.save(old_card);
 				return turn(gameId);
-			}catch(IllegalArgumentException e){
+			}catch(Exception e){
 				model.put("message", e.getMessage());
 				model.put("messageType", "info");
 				return muestraVista(gameId, model);
